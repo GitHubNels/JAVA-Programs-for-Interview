970de6be9be200c849e0692b92183a0ac1d9be4b
Nelson <33600855+GitHubNels@users.noreply.github.com>
2024-11-03 11:54:49 +0530

    Create Stringconcat.java

---
+++ Stringconcat.java
@@ -0,0 +1,38 @@
+
+import org.junit.Test;
+
+public class check
+{
+    @Test
+    public void test()
+    {
+       String str = "45+2*3-";
+       int value = 0;
+        String[] arr = new String[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            arr[i] = (String.valueOf(str.charAt(i)));
+
+         if(arr[i].equals("+"))
+         {
+             int a = Integer.parseInt(arr[0]);
+                int b = Integer.parseInt(arr[1]);
+                value = a + b;
+             System.out.println("Addition operation: "+ value);
+         }
+            if(arr[i].equals("*"))
+            {
+                int c = Integer.parseInt(arr[3]);
+                value = value * c;
+                System.out.println("Multiply: "+ value);
+            }
+            if(arr[i].equals("-"))
+            {
+                int d = Integer.parseInt(arr[5]);
+                value = value - d;
+                System.out.println("Subtract: "+ value);
+            }
+        }
+        System.out.println("Final value: "+ value);
+    }
+
+}
